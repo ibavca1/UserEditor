72d83925cfe5ade6f850f7cfc8180f2812274040
ibavca1 <ibavca@gmail.com>
2023-02-10 15:25:46 +0500

    mv .gitignore

--- UserEditor.ViewModels/MainViewModel.cs
+++ UserEditor.ViewModels/MainViewModel.cs
@@ -1,6 +1,14 @@
+using UserEditor.Shared;
+
 namespace UserEditor.ViewModels;
 
 public class MainViewModel
 {
     public string Gretting => "AVALONIA";
+
+    private ICommon _common;
+    public MainViewModel(ICommon common)
+    {
+        _common = common;
+    }
 }
