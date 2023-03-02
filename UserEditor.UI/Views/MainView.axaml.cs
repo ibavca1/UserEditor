b7ecc21f05467962eed854809e4cfdd3290ac49a
ibavca1 <ibavca@gmail.com>
2023-03-02 13:56:48 +0500

    Test resolve service

--- UserEditor.UI/Views/MainView.axaml.cs
+++ UserEditor.UI/Views/MainView.axaml.cs
@@ -1,11 +1,24 @@
+using Autofac;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using UserEditor.ViewModels;
 
 namespace UserEditor.UI.Views;
 
 public partial class MainView : Window
 {
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+        var builder = new ContainerBuilder();
+        var container = builder.Build();
+        using (var scope = container.BeginLifetimeScope())
+        {
+            var mv = scope.Resolve<MainViewModel>();
+        }
+    }
+
     public MainView()
     {
         InitializeComponent();
@@ -13,9 +26,4 @@ public partial class MainView : Window
         this.AttachDevTools();
 #endif
     }
-
-    /*private void InitializeComponent()
-    {
-        AvaloniaXamlLoader.Load(this);
-    }*/
 }
